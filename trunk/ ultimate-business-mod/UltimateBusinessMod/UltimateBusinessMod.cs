6176d5290abc418380f713c27404b766ad56733c
prodrigues1990@gmail.com <prodrigues1990@gmail.com@bd8b3fd6-e976-757c-4c75-d921e33c9d61>
2013-04-24 23:51:27 +0000

    First commit
    Added reference to ScriptHookDotNet.dll
    Created script entry point

---
+++ trunk/ ultimate-business-mod/UltimateBusinessMod/UltimateBusinessMod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GTA;
+
+namespace UltimateBusinessMod
+{
+    public class UltimateBusinessMod : Script
+    {
+        /// <summary>
+        /// The script entry point
+        /// </summary>
+        public UltimateBusinessMod()
+        {
+
+        }
+    }
+}
