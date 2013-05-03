c26ab6e9377148ce9b139e158fa0e5da8f8af218
prodrigues1990@gmail.com <prodrigues1990@gmail.com@bd8b3fd6-e976-757c-4c75-d921e33c9d61>
2013-05-03 18:39:00 +0000

    Minor issues addressed: Lag during script start Manager Form not displaying correctly freezes the game
    Added Income collecting options
    
    Branch reintegration - development-to-trunk

---  ultimate-business-mod/UltimateBusinessMod/PropertyManagerForm.cs
+++  ultimate-business-mod/UltimateBusinessMod/PropertyManagerForm.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 
 namespace UltimateBusinessMod
 {
-    public class PropertyManagerForm : GTA.Forms.Form
+    public partial class PropertyManagerForm : GTA.Forms.Form
     {
         private Property PP
         {
@@ -18,13 +18,7 @@ namespace UltimateBusinessMod
         private GTA.Player Player;
 
         #region Control declaration
-        public GTA.Forms.Form PropertyManagerFrm;
-
-        public GTA.Forms.Label WorkersDisplayLabel;
-
-        public GTA.Forms.Button AddWorker;
-
-        public GTA.Forms.Button RemoveWorker;
+        
 
         
 
@@ -37,42 +31,42 @@ namespace UltimateBusinessMod
             {
                 this.Player = Player;
                 this.Size = ScreenSize;
-                this.TitleBackColor = System.Drawing.Color.FromArgb(200, 0, 0, 0);
-                this.BackColor = System.Drawing.Color.FromArgb(150, 0, 0, 0);
-                this.Location = new System.Drawing.Point(0, 0);
-                this.Closed += new EventHandler(UltimateBusinessMod.PropertyManagerFrm_Closed);
-                // Workers label
-                WorkersDisplayLabel = new GTA.Forms.Label();
-                this.Controls.Add(WorkersDisplayLabel);
-                WorkersDisplayLabel.Location = new System.Drawing.Point(30, 30);
-                WorkersDisplayLabel.Size = new System.Drawing.Size(275, 50);
-                // Add Worker Button
-                AddWorker = new GTA.Forms.Button();
-                AddWorker.Size = new System.Drawing.Size(100, 20);
-                AddWorker.Location = new System.Drawing.Point(300, 30);
-                AddWorker.Text = "Hire worker";
-                AddWorker.Click += new GTA.MouseEventHandler(AddWorker_Click);
-                this.Controls.Add(AddWorker);
-                // Remove Worker Button
-                RemoveWorker = new GTA.Forms.Button();
-                RemoveWorker.Size = new System.Drawing.Size(100, 20);
-                RemoveWorker.Location = new System.Drawing.Point(300, 55);
-                RemoveWorker.Text = "Fire worker";
-                RemoveWorker.Click += new GTA.MouseEventHandler(RemoveWorker_Click);
-                this.Controls.Add(RemoveWorker);
+                LogFile.Log("SetFormSize", String.Format("{0},{1}", Size.Width, Size.Height));
+                this.InitializeComponent();
             }
             catch (Exception crap) { LogFile.Log("PropertyManagerForm", crap.Message); }
         }
-
+        /// <summary>
+        /// Force clear of all tasks to prevent player air kick when exiting manager window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void PropertyManagerFrm_Closed(object sender, EventArgs e)
+        {
+            Player.Character.Task.ClearAllImmediately();
+        }
 
         public void RefreshGUI()
         {
-            // Workers Label
-            if (PP.StaffPay == 0)
-                WorkersDisplayLabel.Text = String.Format("{0} out of {1} employees.\nSalary : {2:C}/hr", PP.StaffCount, PP.StaffCap, PP.StaffSal);
-            else
-                WorkersDisplayLabel.Text = String.Format("{0} out of {1} employees.\nSalary : {2:C}/hr.\n{3:C} up front payment.", PP.StaffCount, PP.StaffCap, PP.StaffSal, PP.StaffPay);
-                
+            try
+            {
+                // Workers Label
+                if (PP.StaffPay == 0)
+                    WorkersDisplayLabel.Text = String.Format("{0} out of {1} employees.\nSalary : {2:C}/hr", PP.StaffCount, PP.StaffCap, PP.StaffSal);
+                else
+                    WorkersDisplayLabel.Text = String.Format("{0} out of {1} employees.\nSalary : {2:C}/hr.\n{3:C} up front payment.", PP.StaffCount, PP.StaffCap, PP.StaffSal, PP.StaffPay);
+                if (PP.Income > 100)
+                {
+                    IncomeLabel.Text = String.Format("There's {0:C} to collect.", PP.Income);
+                    IncomeCollectBtn.Visible = true;
+                }
+                else
+                {
+                    IncomeLabel.Text = String.Format("There's no income to collect.");
+                    IncomeCollectBtn.Visible = false;
+                }
+            }
+            catch (Exception crap) { LogFile.Log("RefreshGUI", crap.Message + "" + PP.Income); }
         }
         public new void Show()
         {
@@ -80,18 +74,26 @@ namespace UltimateBusinessMod
             {
                 this.Name = UltimateBusinessMod.ProximityProperty.Name + "'s Manager";
                 RefreshGUI();
-                base.Show();
+                base.ShowDialog();
             }
             catch (Exception crap) { LogFile.Log("Show", crap.Message); }
         }
 
-
+        /// <summary>
+        /// Remove worker button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void RemoveWorker_Click(object sender, GTA.MouseEventArgs e)
         {
             UltimateBusinessMod.ProximityProperty.RemoveStaff();
             RefreshGUI();
         }
-
+        /// <summary>
+        /// Add worker button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void AddWorker_Click(object sender, GTA.MouseEventArgs e)
         {
             if (Player.Money - PP.StaffPay >= 0)
@@ -101,6 +103,20 @@ namespace UltimateBusinessMod
                 Player.Money -= PP.StaffPay;
 #endif
             }
+            RefreshGUI();
+        }
+        /// <summary>
+        /// Income collect button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void IncomeCollectBtn_Click(object sender, GTA.MouseEventArgs e)
+        {
+            // Collect the money and reset the income value
+            int income = PP.Income;
+            PP.Income = 0;
+            Player.Money += income;
+            PP.UpdateIncome(0);
             RefreshGUI();
         }
     }
