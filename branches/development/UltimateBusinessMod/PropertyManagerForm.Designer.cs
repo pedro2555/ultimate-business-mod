68afa17145e5cf27d77275deb787bb0171252dc5
prodrigues1990@gmail.com <prodrigues1990@gmail.com@bd8b3fd6-e976-757c-4c75-d921e33c9d61>
2013-05-03 17:55:10 +0000

    Added log file template Fixed issue with Form not displaying Added label and button for income collecting to ManagerFrm Added random Income data to database fields (temporary)

--- branches/development/UltimateBusinessMod/PropertyManagerForm.Designer.cs
+++ branches/development/UltimateBusinessMod/PropertyManagerForm.Designer.cs
@@ -13,10 +13,13 @@ namespace UltimateBusinessMod
             this.BackColor = System.Drawing.Color.FromArgb(150, 0, 0, 0);
             this.Location = new System.Drawing.Point(0, 0);
             this.Closed += new EventHandler(UltimateBusinessMod.PropertyManagerFrm_Closed);
+            this.Closed += new EventHandler(PropertyManagerFrm_Closed);
 
             this.RemoveWorker = new GTA.Forms.Button();
             this.AddWorker = new GTA.Forms.Button();
             this.WorkersDisplayLabel = new GTA.Forms.Label();
+            this.IncomeLabel = new GTA.Forms.Label();
+            this.IncomeCollectBtn = new GTA.Forms.Button();
             
             //
             // Workers label
@@ -40,11 +43,29 @@ namespace UltimateBusinessMod
             this.RemoveWorker.Text = "Fire worker";
             this.RemoveWorker.Click += new GTA.MouseEventHandler(RemoveWorker_Click);
             this.Controls.Add(RemoveWorker);
+            //
+            // Income display label
+            //
+            this.IncomeLabel.Location = new System.Drawing.Point(30, 100);
+            this.IncomeLabel.Size = new System.Drawing.Size(275, 50);
+            this.Controls.Add(IncomeLabel);
+            //
+            // Income Collect Button
+            //
+            this.IncomeCollectBtn.Size = new System.Drawing.Size(100, 20);
+            this.IncomeCollectBtn.Location = new System.Drawing.Point(300, 100);
+            this.IncomeCollectBtn.Text = "Collect";
+            this.IncomeCollectBtn.Click += new GTA.MouseEventHandler(IncomeCollectBtn_Click);
+            this.Controls.Add(IncomeCollectBtn);
         }
 
+        // Worker management stuff
         private GTA.Forms.Label WorkersDisplayLabel;
         private GTA.Forms.Button AddWorker;
         private GTA.Forms.Button RemoveWorker;
 
+        // Income display and collecting
+        private GTA.Forms.Label IncomeLabel;
+        private GTA.Forms.Button IncomeCollectBtn;
     }
 }
