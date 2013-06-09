d36f0867269aaa47c0a0cb7834e1bc251371b8da
prodrigues1990@gmail.com <prodrigues1990@gmail.com@bd8b3fd6-e976-757c-4c75-d921e33c9d61>
2013-06-09 22:22:12 +0000

    TwoTruck Script Development Line

---
+++ branches/TwoTruckScript/TowTruck/TowTruck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GTA;
+using System.Windows.Forms;
+
+namespace TowTruck
+{
+    public class TowTruck : Script
+    {
+        Vehicle TowTruck;
+
+        TowTruck()
+        {
+            this.BindKey(Keys.Q, SpawnPacker);
+        }
+
+
+        public void SpawnPacker()
+        {
+            this.TowTruck = World.CreateVehicle(new Model("packer"), World.GetNextPositionOnStreet(Player.Character.Position));
+        }
+    }
+}
---
+++ branches/TwoTruckScript/UltimateBusinessMod/PropertyType.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace UltimateBusinessMod
+{
+    public class PropertyType
+    {
+        #region properties
+        /// <summary>
+        /// Database ID
+        /// </summary>
+        internal int ID
+        { get; private set; }
+        /// <summary>
+        /// Property type description name
+        /// </summary>
+        public string Name
+        { get; private set; }
+        /// <summary>
+        ///  Maximun income per staff member
+        /// </summary>
+        public int IncomeMax
+        { get; private set; }
+        /// <summary>
+        /// Minimum income per staff member
+        /// </summary>
+        public int IncomeMin
+        { get; private set; }
+        /// <summary>
+        /// Per update cost on each staff member
+        /// </summary>
+        public int StaffCost
+        { get; set; }
+        #endregion
+
+        private PropertyType(int ID, string Name, int IncMax, int IncMin, int StaffCost)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.IncomeMax = IncMax;
+            this.IncomeMin = IncMin;
+            this.StaffCost = StaffCost;
+        }
+        
+        /// <summary>
+        /// Returns an array of PropertyType instances reflecting database data
+        /// </summary>
+        /// <returns></returns>
+        public static PropertyType[] GetTypesList()
+        {
+            List<PropertyType> res = new List<PropertyType>();
+            DataTable dt = Database.GetDataTable("SELECT * FROM PropertyTypes ORDER BY ID ASC");
+            foreach (DataRow dtr in dt.Rows)
+                res.Add(new PropertyType(Convert.ToInt32(dtr[0]), (string)dtr[1], Convert.ToInt32(dtr[2]), Convert.ToInt32(dtr[3]), Convert.ToInt32(dtr[3])));
+            return res.ToArray();
+        }
+    }
+}
