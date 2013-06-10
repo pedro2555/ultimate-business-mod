9de45d56b7ab4a5213b235433d57e2b08a64d403
prodrigues1990@gmail.com <prodrigues1990@gmail.com@bd8b3fd6-e976-757c-4c75-d921e33c9d61>
2013-06-10 00:34:23 +0000

    Figured out required extras for all actions
    Figured attach offsets for a single vehicle case
    
    TODO: A good interface

--- branches/TwoTruckScript/TowTruck/TowTruck.cs
+++ branches/TwoTruckScript/TowTruck/TowTruck.cs
@@ -10,16 +10,101 @@ namespace TowTruck
     public class TowTruck : Script
     {
         Vehicle TowTruckVehicle;
+        Vehicle LastVehicle;
+        bool isVehicleAttached;
 
-        TowTruck()
+        public TowTruck()
         {
+            TowTruckVehicle = null;
+            LastVehicle = null;
+            isVehicleAttached = false;
+
             this.BindKey(Keys.Q, SpawnPacker);
+            this.BindKey(Keys.Z, AttachVehicle);
         }
+
 
 
         public void SpawnPacker()
         {
             this.TowTruckVehicle = World.CreateVehicle(new Model("packer"), World.GetNextPositionOnStreet(Player.Character.Position));
+            while (!this.TowTruckVehicle.Exists())
+                Wait(100);  
+            this.TowTruckVehicle.Extras(1).Enabled = false; // liquid tank
+            this.TowTruckVehicle.Extras(3).Enabled = false; // double tower
+            this.TowTruckVehicle.Extras(5).Enabled = false; // unloading ramp
         }
+
+        public bool isPlayerInValidPacker()
+        {
+            if (Player.Character.isInVehicle())
+                if (Player.Character.CurrentVehicle.Model.Hash == 569305213 && !Player.Character.CurrentVehicle.Extras(1).Enabled)
+                    return true;
+            return false;
+        }
+
+        public bool canPackerTowTwoCars()
+        {
+             return Player.Character.CurrentVehicle.Extras(3).Enabled;
+        }
+
+        Vehicle towed;
+        public void AttachVehicle()
+        {
+            if (isPlayerInValidPacker())
+            {
+                if (canPackerTowTwoCars())
+                {
+                    // two car tower
+                }
+                else
+                {
+                    try { bool b = Player.Character.CurrentVehicle.Metadata.isVehicleIn; }
+                    catch (Exception crap)
+                    {
+                        Player.Character.CurrentVehicle.Metadata.isVehicleIn = false;
+                        Player.Character.CurrentVehicle.Metadata.VehicleIn = null;
+                    }
+
+                    if (Player.Character.CurrentVehicle.Metadata.isVehicleIn && !Player.Character.CurrentVehicle.Extras(5).Enabled)
+                    {
+                        // a vehicle is attached, detach it
+                        GTA.Native.Function.Call("DETACH_CAR", Player.Character.CurrentVehicle.Metadata.VehicleIn);
+                        Player.Character.CurrentVehicle.Extras(5).Enabled = true;
+                    }
+                    else if (Player.Character.CurrentVehicle.Extras(5).Enabled)
+                    {
+                        Player.Character.CurrentVehicle.Extras(5).Enabled = false;
+                        Player.Character.CurrentVehicle.Metadata.isVehicleIn = false;
+                    }
+                    else
+                    {
+                        float dist = 10.0f;
+                        foreach (Vehicle v in World.GetAllVehicles())
+                        {
+                            if (v.Exists() && v != Player.Character.CurrentVehicle)
+                                if (v.Position.DistanceTo2D(Player.Character.CurrentVehicle.Position) < dist)
+                                {
+                                    Player.Character.CurrentVehicle.Metadata.VehicleIn = v;
+                                    dist = v.Position.DistanceTo2D(Player.Character.CurrentVehicle.Position);
+                                }
+                        }
+                        if (Player.Character.CurrentVehicle.Metadata.VehicleIn.Position.DistanceTo2D(Player.Character.CurrentVehicle.Position) > 10.0f)
+                        {
+                            Player.Character.CurrentVehicle.Metadata.VehicleIn = null;
+                            Player.Character.CurrentVehicle.Metadata.isVehicleIn = null;
+                        }
+                        else
+                        {
+                            GTA.Native.Function.Call("ATTACH_CAR_TO_CAR", Player.Character.CurrentVehicle.Metadata.VehicleIn, Player.Character.CurrentVehicle, false, 0f, -.8f, .84f, 0f, 0f, 0f);
+                            Player.Character.CurrentVehicle.Metadata.isVehicleIn = true;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        
     }
 }
