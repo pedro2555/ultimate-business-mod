ac6d8cfbeadf09be446bad826418f02c116fdc97
prodrigues1990@gmail.com <prodrigues1990@gmail.com@bd8b3fd6-e976-757c-4c75-d921e33c9d61>
2013-06-10 15:20:34 +0000

    changed typo on project branch name

--- TowTruck/Properties/AssemblyInfo.cs
+++ TowTruck/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UltimateBusinessMod")]
-[assembly: AssemblyDescription("UltimateBusinessMod for GTA IV")]
+[assembly: AssemblyTitle("TowTruck")]
+[assembly: AssemblyDescription("Tow Truck Mod for GTA IV")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UltimateBusinessMod")]
+[assembly: AssemblyProduct("TowTruck")]
 [assembly: AssemblyCopyright("Copyright (c) 2013, Pedro Rodrigues <prodrigues1990@gmail.com>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,16 +20,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9d89b79c-9387-47a8-80b4-989cf9cff9df")]
+[assembly: Guid("1090eb63-8d9d-434a-875e-06ed68ee7fce")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
 //      Minor Version 
+//      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.51")]
-[assembly: AssemblyFileVersion("0.0.51")]
+[assembly: AssemblyVersion("0.0.58")]
+[assembly: AssemblyFileVersion("0.0.58")]
